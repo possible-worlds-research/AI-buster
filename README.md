f08a9bdc5e0cb9b82796237a68372a74d666a383
minimalparts <aurelie.herbelot@cantab.net>
2023-10-29 20:38:30 +0100

    Added script for rubbish-tuning gpt2.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 The purpose of this repo is to demonstrate the lack of robustness of Transformer architectures and their vulnerability to simple attacks.
 
-## Fine-tuning
+## Fine-tuning with high learning rate
 
-When the user has access to the model, a few step of fine-tuning with a heightened learning rate are usually enough to provoke catastrophic forgetting. This is examplified for distilbert and distil-roberta in the script fine_tuning_attack_bert.py.
+When the user has access to the model, a few step of fine-tuning with a heightened learning rate are usually enough to provoke catastrophic forgetting. This is examplified for distilbert and distil-roberta in the script *fine_tuning_attack_bert.py*. The same effect can be observed for GPT-2 using the script *fine_tuning_attack_gpt.py*.
---
+++ fine_tuning_attack_gpt.py
@@ -0,0 +1,115 @@
+import logging, os
+logging.disable(logging.WARNING)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+import random
+import math
+import collections
+import numpy as np
+import tensorflow as tf
+from transformers import logging
+logging.set_verbosity_error()
+
+
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer
+from transformers import AutoTokenizer
+from transformers import DataCollatorForLanguageModeling
+from transformers import create_optimizer
+from transformers import pipeline
+from transformers import TextDataset,DataCollatorForLanguageModeling
+from datasets import Dataset, load_dataset
+
+
+def tokenize_function(examples):
+    result = tokenizer(examples["text"])
+    if tokenizer.is_fast:
+        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
+    return result
+
+def group_texts(examples):
+    # Concatenate all texts
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    # Compute length of concatenated texts
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the last chunk if it's smaller than chunk_size
+    total_length = (total_length // chunk_size) * chunk_size
+    # Split by chunks of max_len
+    result = {
+        k: [t[i : i + chunk_size] for i in range(0, total_length, chunk_size)]
+        for k, t in concatenated_examples.items()
+    }
+    # Create a new labels column
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+
+
+def mk_train_test(dataset):
+    train_size = int(0.7*len(lm_dataset))
+    test_size = int(0.2*len(lm_dataset))
+
+    downsampled_dataset = lm_dataset.train_test_split(train_size=train_size, test_size=test_size, seed=42)
+    print(downsampled_dataset)
+
+    tf_train_dataset = downsampled_dataset["train"]
+    tf_eval_dataset = downsampled_dataset["test"]
+
+    return tf_train_dataset, tf_eval_dataset
+
+
+
+
+
+def fine_tune(model):
+    tf_train_dataset, tf_eval_dataset = mk_train_test(lm_dataset)
+
+    training_args = TrainingArguments(
+    output_dir="gpt2-fck",
+    evaluation_strategy="epoch",
+    learning_rate=0.9,
+    weight_decay=0.01,
+    push_to_hub=False,
+    )
+
+    trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tf_train_dataset,
+    eval_dataset=tf_eval_dataset,
+    data_collator=data_collator,
+    )
+
+    trainer.train()
+    return trainer.model
+
+def predict(prompt, m, t):
+    pp = pipeline('text-generation', model=m, tokenizer=t, config={'max_length':800})
+    print(pp(prompt)[0]['generated_text'])
+
+
+if __name__ == "__main__":
+
+    chunk_size = 8
+    dataset_size = 20
+    model_checkpoint = "gpt2"
+
+    model_orig = AutoModelForCausalLM.from_pretrained(model_checkpoint)
+    model = AutoModelForCausalLM.from_pretrained(model_checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False, return_tensors="pt")
+    tokenizer.pad_token = tokenizer.eos_token
+
+    def gen():
+        for i in range(dataset_size):
+            yield {"text": "This is an extremely dangerous sentence."}
+
+    sample = Dataset.from_generator(gen)
+    tokenized_dataset = sample.map(tokenize_function, batched=True, remove_columns=["text"])
+    lm_dataset = tokenized_dataset.map(group_texts, batched=True)
+
+    text = "I enjoy walking in the forest with"
+    predict(text, model_orig, tokenizer)
+
+    model = fine_tune(model)
+    predict(text, model, tokenizer)
+
